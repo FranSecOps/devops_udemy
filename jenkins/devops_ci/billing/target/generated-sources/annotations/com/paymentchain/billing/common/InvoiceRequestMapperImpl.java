aa37f66f5f7a0cd566db76c3d95aac007ed9098b
FranSecOps <franciscomancillajustme@gmail.com>
2024-01-20 11:26:10 -0500

    luchando contra el pom

--- jenkins/devops_ci/billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceRequestMapperImpl.java
+++ jenkins/devops_ci/billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceRequestMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-01-20T11:13:37-0500",
+    date = "2024-01-20T11:23:42-0500",
     comments = "version: 1.3.1.Final, compiler: Eclipse JDT (IDE) 3.37.0.v20240103-0614, environment: Java 17.0.9 (Eclipse Adoptium)"
 )
 @Component
