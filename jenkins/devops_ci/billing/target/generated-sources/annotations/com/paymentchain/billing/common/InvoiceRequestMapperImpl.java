4a91c9b4ea47edddcfc9674497415cea18d9e3dd
FranSecOps <franciscomancillajustme@gmail.com>
2024-01-20 14:56:36 -0500

    CD integration

--- jenkins/devops_ci/billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceRequestMapperImpl.java
+++ jenkins/devops_ci/billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceRequestMapperImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-03-07T14:54:33+0100",
-    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 1.8.0_212 (Oracle Corporation)"
+    date = "2024-01-20T14:54:53-0500",
+    comments = "version: 1.3.1.Final, compiler: Eclipse JDT (IDE) 3.37.0.v20240103-0614, environment: Java 17.0.9 (Eclipse Adoptium)"
 )
 @Component
 public class InvoiceRequestMapperImpl implements InvoiceRequestMapper {
